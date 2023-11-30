8693a2e7ccba668ff235af9e7e74d42a6af64f21
mariana <marianadiniz.v@gmail.com>
2023-11-29 21:32:25 -0300

    implementando controller advice

---
+++ api/api/src/main/java/med/voll/api/adapters/controllers/exception/TrataExceptions.java
@@ -0,0 +1,33 @@
+package med.voll.api.adapters.controllers.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TrataExceptions {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity trataErro404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity trataErro400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new));
+    }
+
+    private record DadosErroValidacao(String campo, String message){
+        private DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
